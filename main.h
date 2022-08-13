354f97cd68ef7873c1bd85e22ef744159184c780
Kate Navisino <107048843+Navisino@users.noreply.github.com>
2022-08-13 20:43:20 +0300

    Update main.h

--- main.h
+++ main.h
@@ -74,7 +74,15 @@ int _puts(char *str);
 int print_rot13(va_list l, flags_t *f);
 int print_rev(va_list l, flags_t *f);
 int print_bigS(va_list l, flags_t *f);
-count += _putchar(s[i]);
+
+int print_percent(va_list l, flags_t *f)
+{
+	(void)l;
+	(void)f;
+	return (_putchar('%'));
+}
+
+
 
 
 int print_address(va_list l, flags_t *f);

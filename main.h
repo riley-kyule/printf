28357a6df6bc7c3b16b05a72714313cbf1d1bd50
Phillip Kyule <66269321+riley-kyule@users.noreply.github.com>
2022-08-13 13:23:12 +0300

    Update main.h

--- main.h
+++ main.h
@@ -53,6 +53,48 @@ typedef struct printHandler
 	int (*f)(va_list ap, flags_t *f);
 } ph;
 
+int print_int(va_list l, flags_t *f);
+void print_number(int n);
+int print_unsigned(va_list l, flags_t *f);
+int count_digit(int i);
+
+
+int print_hex(va_list l, flags_t *f);
+int print_hex_big(va_list l, flags_t *f);
+int print_binary(va_list l, flags_t *f);
+int print_octal(va_list l, flags_t *f);
+
+
+char *convert(unsigned long int num, int base, int lowercase);
+
+
+int _printf(const char *format, ...);
+
+
+int (*get_print(char s))(va_list, flags_t *);
+
+
+int get_flag(char s, flags_t *f);
+
+
+int print_string(va_list l, flags_t *f);
+int print_char(va_list l, flags_t *f);
+
+
+int _putchar(char c);
+int _puts(char *str);
+
+
+int print_rot13(va_list l, flags_t *f);
+int print_rev(va_list l, flags_t *f);
+int print_bigS(va_list l, flags_t *f);
+
+
+int print_address(va_list l, flags_t *f);
+
+
+int print_percent(va_list l, flags_t *f);
+
 int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i,
 va_list list, char buffer[], int flags, int width, int precision, int size);

92af8fe57dda423e3b372b4df5b0b1750599f703
Kate Navisino <107048843+Navisino@users.noreply.github.com>
2022-08-17 13:40:44 +0300

    Update get_print.c

--- get_print.c
+++ get_print.c
@@ -3,7 +3,7 @@
 /**
  * get_print - selects the right printing function
  * depending on the conversion specifier passed to _printf
- * @s: character that holds the conversion specifier
+ * @s: character that holds the conversion specified
  * Description: the function loops through the structs array
  * func_arr[] to find a match between the specifier passed to _printf
  * and the first element of the struct, and then the approriate
